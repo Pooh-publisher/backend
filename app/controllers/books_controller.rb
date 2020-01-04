e8dacfc1306e55516357c00338681802e5d85736
trambui <ngoc4@ntu.edu.sg>
2020-01-04 21:15:09 +0800

    Add book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,47 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to action: :index
+    else
+      render :new
+    end
+  end
+
+  def index
+    @books = Book.all.order(:id)
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to action: :index
+    else 
+      render :edit 
+    end  
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to action: :index
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :description, :expected_completion_date, :category, :genre)
   end
 end
